63cc16106f7c86feefb5cce2f06e29f5a90fcda2
jkchappydev <jkchappydev@gmail.com>
2025-06-28 19:32:06 +0900

    study: section2 - 프로젝트 생성

--- src/test/java/com/studyquerydsl/StudyQuerydslApplicationTests.java
+++ src/test/java/com/studyquerydsl/StudyQuerydslApplicationTests.java
@@ -1,13 +1,39 @@
 package com.studyquerydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.studyquerydsl.entity.Hello;
+import com.studyquerydsl.entity.QHello;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@ActiveProfiles(value = "local")
+@Transactional
 class StudyQuerydslApplicationTests {
+
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        // querydsl 사용하려면 JPAQueryFactory 필요함
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query.
+                selectFrom(qHello)
+                .fetchOne();
+
+        Assertions.assertThat(result).isEqualTo(hello);
+        Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
