4aaf8f5be98749eb4c85e8dc009fd8e61726912d
jkchappydev <jkchappydev@gmail.com>
2025-06-28 20:39:31 +0900

    study: section3 - 예제 도메인 모델과 동작확인

---
+++ src/main/java/com/studyquerydsl/entity/Team.java
@@ -0,0 +1,34 @@
+package com.studyquerydsl.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+}
